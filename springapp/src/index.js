8fca0d058a4b54fd3caddf1504eb7034b05237f6
Iamneo <techadmin@iamneo.ai>
2023-08-04 19:04:19 +0000

    app

---
+++ springapp/src/index.js
@@ -0,0 +1,75 @@
+console.log("FDSAgasgd")
+console.log("FDSAgasgd")
+console.log("FDSAgasgd")
+console.log("FDSAgasgd")
+console.log("FDSAgasgd")
+console.log("FDSAgasgd")
+console.log("FDSAgasgd")
+console.log("FDSAgasgd")
+console.log("FDSAgasgd")
+console.log("FDSAgasgd")
+console.log("FDSAgasgd")
+console.log("FDSAgasgd")
+console.log("FDSAgasgd")
+console.log("FDSAgasgd")
+console.log("FDSAgasgd")
+console.log("FDSAgasgd")
+console.log("FDSAgasgd")
+console.log("FDSAgasgd")
+console.log("FDSAgasgd")
+console.log("FDSAgasgd")
+
+
+for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}
+
+
+for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}for(let i = 0; i <= 100; i++) {
+    console.log("Fadsf")
+}
